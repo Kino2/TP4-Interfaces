4cde6972a2a83ca88679ce8b6f26d000dc6c06d2
TuzioDj <90795509+TuzioDj@users.noreply.github.com>
2023-11-25 19:11:08 -0300

    Animacion navbar

--- js/app.js
+++ js/app.js
@@ -1,20 +1,35 @@
-window.addEventListener('scroll', function () {
-    var titulo = document.getElementById('titulo');
-    var navbar = document.querySelector('.navbar');
-    var scrollPosition = window.scrollY;
-    var navbarHeight = navbar.offsetHeight;
+//Punto 5
+// window.addEventListener('scroll', function () {
+//     var titulo = document.getElementById('titulo');
+//     var navbar = document.querySelector('.navbar');
+//     var scrollPosition = window.scrollY;
+//     var navbarHeight = navbar.offsetHeight;
 
-    // Si el scroll supera la altura de la navbar, ajusta el tamaño y posición del título
-    if (scrollPosition > navbarHeight) {
-        titulo.style.width = '200px'; // Cambia el ancho a 200px (o el tamaño que desees)
-        titulo.style.right = '50%'; // Centra horizontalmente
-        titulo.style.transform = 'translateX(50%) translateY(-50%)'; // Centra verticalmente
-    } else {
-        titulo.style.width = '520px'; // Vuelve al ancho original
-        titulo.style.right = '31%'; // Vuelve a la posición original
-        titulo.style.transform = 'none'; // Elimina la transformación
+//     // Si el scroll supera la altura de la navbar, ajusta el tamaño y posición del título
+//     if (scrollPosition > navbarHeight/2) {
+//         titulo.style.width = '200px'; // Cambia el ancho a 200px (o el tamaño que desees)
+//         titulo.style.right = '50%'; // Centra horizontalmente
+//         titulo.style.transform = 'translateX(50%) translateY(-50%)'; // Centra verticalmente
+//     } else {
+//         titulo.style.width = '520px'; // Vuelve al ancho original
+//         titulo.style.right = '31%'; // Vuelve a la posición original
+//         titulo.style.transform = 'none'; // Elimina la transformación
+//     }
+// });
+    const navbar = document.querySelector(".navbar");
+    const logoGrande = document.querySelector("#titulo");
+    const logoChico = document.querySelector(".navbarMedio");
+    document.addEventListener("scroll", () => {
+    if (window.scrollY < 103) {
+        logoChico.style.opacity = 0;
     }
-});
+    if (window.scrollY > 103) {
+        logoChico.style.opacity = window.scrollY / 400;
+    }
+        logoGrande.style.opacity = 1 - window.scrollY / 120;
+        const scaleValue = Math.max(1 - window.scrollY / 200, 0.0);
+        logoGrande.style.scale = scaleValue;
+    });
 
 //Punto 6 
 window.addEventListener('scroll', function () {
@@ -55,6 +70,7 @@ const cargarElem = () => {
         c.style.transition = `opacity 0.5s ease ${index * 0.2}s`; // Ajusta la duración y el retraso de la transición
         c.style.opacity = '1';
     });
+    setTimeout(() => {logo.style.transition = ``}, 1000);
 };
 // Llama a la función después de 1 segundo
 setTimeout(cargarElem, 500);
